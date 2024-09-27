8e7ad1bb67760e214392ae28101f0c8a3b35fb31
nonamenoname <bubkin-007@ya.ru>
2024-09-27 12:18:22 +0300

    tests

---
+++ GigsChatClientTest/Program.cs
@@ -0,0 +1,9 @@
+﻿var handler = new HttpClientHandler()
+{
+    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+};
+var secretKey = "";
+var httpClient = new HttpClient(handler);
+var GigaChat = new GigaChat(httpClient, secretKey);
+var answer = GigaChat.BlindQuestion("как дела?");
+Console.WriteLine(answer);
